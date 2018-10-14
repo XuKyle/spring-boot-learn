59c67752f7ce9079855c5033b8a3a95aab29d8d8
kyle.xu <673296525@qq.com>
2018-10-14 13:38:59 +0800

    springboot 分布式限流

---
+++ springboot-limit/src/main/java/com/kylexu/springbootlimit/SpringbootLimitApplication.java
@@ -0,0 +1,12 @@
+package com.kylexu.springbootlimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLimitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLimitApplication.class, args);
+    }
+}
---
+++ springboot-limit/src/test/java/com/kylexu/springbootlimit/SpringbootLimitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kylexu.springbootlimit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootLimitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
